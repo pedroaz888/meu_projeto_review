0accbfdd7f9a7c17cefa191b63afa22d38cc538d
PedroAz <pdegutty@gmail.com>
2025-12-29 16:27:13 -0300

    deploy: config

--- projeto/settings.py
+++ projeto/settings.py
@@ -90,11 +90,15 @@ WSGI_APPLICATION = 'projeto.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # DATABASES = {
 #         'default': {
@@ -107,10 +111,6 @@ DATABASES = {
           
 #      }
 #  }
-
-# DATABASES = {
-#   'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-# }
 
 
 
