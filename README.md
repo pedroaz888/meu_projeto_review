21748ed63a25d33b0f02715229cd58236873fec2
PedroAz <pdegutty@gmail.com>
2025-12-29 18:56:41 -0300

    Revert "Configurar armazenamento de mídia no AWS S3 para produção"
    This reverts commit cb0e9716cb0d1db7ce6c8dd1145be925e5f1ed98.

--- README.md
+++ README.md
@@ -1,28 +1,8 @@
 # meu_projeto_review
 
-Projeto CRUD para lembrar todo o processo de codificação no python usando o
+Projeto CRUD para lembrar todo o processo de codificação no python usando o 
 django e postgres como banco de dados, e fazendo deploy pelo render.
 com filtro básico que busca a partir de um valor.
 
 https://meu-projeto-review.onrender.com/stok/cadastrar_produto/
-
-## Configuração para Produção
-
-### Armazenamento de Mídia (Fotos)
-
-As fotos dos produtos são armazenadas no AWS S3 em produção. Para configurar:
-
-1. Crie um bucket no AWS S3
-2. Configure as seguintes variáveis de ambiente no Render:
-   - `AWS_ACCESS_KEY_ID`: Sua chave de acesso AWS
-   - `AWS_SECRET_ACCESS_KEY`: Sua chave secreta AWS
-   - `AWS_STORAGE_BUCKET_NAME`: Nome do seu bucket S3
-   - `AWS_S3_REGION_NAME`: Região do bucket (padrão: us-east-1)
-
-### Como executar localmente
-
-1. Clone o repositório
-2. Instale as dependências: `pip install -r requirements.txt`
-3. Configure o arquivo `.env` com suas variáveis
-4. Execute: `python manage.py runserver`
 
--- projeto/settings.py
+++ projeto/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'stok',
-    'storages',
+   
 ]
 
 MIDDLEWARE = [
@@ -162,23 +162,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# AWS S3 Configuration for Media Files
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID', default=None)
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY', default=None)
-AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME', default=None)
-AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME', default='us-east-1')
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-AWS_DEFAULT_ACL = 'public-read'
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-
-# Use S3 for media files if AWS credentials are provided
-if AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY and AWS_STORAGE_BUCKET_NAME:
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
-else:
-    # Fallback to local storage (for development)
-    MEDIA_URL = '/media/'
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
